5fab4fb4bc8790315a66b052b2a7d102dfb7ec9c
Sunny Gandhwani <sunnygandhwani027@gmail.com>
2025-04-23 23:31:55 +0530

    init basic server setup

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,15 @@
-console.log('hello world, how are you?');
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+
+app.get('/', (_, res): any => res.send('Hello World'));
+
+app.listen(3000, () => {
+  console.log('Server is running on port http://localhost:3000');
+});
 
